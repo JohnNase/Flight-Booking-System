46aae3a3f39be0193fff6c9ad8910376fef8f9ca
sara-berberi <sberberi21@gmail.com>
2023-06-07 21:37:30 +0200

    logo removal

--- HTML/memberships/navbar.php
+++ HTML/memberships/navbar.php
@@ -11,7 +11,7 @@
       <li><a id="home-btn" href="http://localhost/Flight-Booking-System/index.php" color="white">HOME</a></li>
       <li><a id="login-btn" href="http://localhost/Flight-Booking-System/HTML/login.php" color="white">LOGIN</a></li>
       <li><a id="register-btn" href="http://localhost/Flight-Booking-System/HTML/signup.php" color="white">SIGN UP</a></li>
-      <li><a id="availableflights-btn" href="http://localhost/Flight-Booking-System/HTML/availableflights.php" color="white">SEARCH FLIGHTS</a></li>
+      <li><a id="availableflights-btn" href="http://localhost/Flight-Booking-System/HTML/availableFlights.php" color="white">SEARCH FLIGHTS</a></li>
       <li><a id="contactus-btn" href="http://localhost/Flight-Booking-System/HTML/contactus.php" color="white">CONTACT US</a></li>
       <li><a id="packages-btn" href="http://localhost/Flight-Booking-System/HTML/packages.php" color="white">PACKAGES</a></li>
       <li><a id="aboutus-btn" href="http://localhost/Flight-Booking-System/HTML/aboutus.php" color="white">ABOUT US</a></li>
